6e3db51a869bbf11fd256c197f6061eab537024e
Eric Comitz <ericcomitz@yahoo.com>
2015-11-23 00:09:35 -0500

    File management and List class
    Title

---
+++ implementation/src/List.java
@@ -0,0 +1,31 @@
+/**
+ * Created by eric on 10/28/2015.
+ */
+public class List { // This class handled by Eric
+public List(Items items) // Doubly Linked List
+{
+    head = null;
+    tail = null;
+
+}
+    Node tail;
+    Node head;
+class Node{
+    Node previous;
+    Items itemInside;
+    Node next;
+
+}
+
+public class Items{ // This class handled by Martin
+
+public Items(String name, int quantity)
+{
+
+}
+
+
+
+}
+
+}
---
+++ implementation/src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * This class needed for primary functions.
+ */
+public class Main {
+
+
+    public static void main(String[] args)
+    {
+
+    }
+
+}
