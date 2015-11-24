a09f229bc47402852d0f25a20ce02a5eb182894c
merrellblack <blackm1@g.cofc.edu>
2015-11-24 15:57:11 -0500

    initial commit  for discussion -temp src 11/24

--- implementation/src/Users.java
+++ implementation/src/Users.java
@@ -1,10 +1,67 @@
-/**
- * Created by eric on 10/28/2015.
- */
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class Users {
 
-    public Users(String username, String password)
-    {}
-    // public Users(String username, String password, int phoneNumber) {} Android version.
+
+    String username;
+    String password;
+    InetAddress ip;
+    String hostname;
+    String ipAddr;
+
+    public Users(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+
+
+
+
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username){
+        this.username = username;
+
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    // attempt to automatically pull the local ip address and host name
+    public void autoAssignNet() {
+        try {
+            ip = InetAddress.getLocalHost();
+            ipAddr = ip.getHostAddress();
+            hostname = ip.getHostName();
+            System.out.println("Your current IP address : " + ip);
+            System.out.println("Your current Hostname : " + hostname);
+
+        } catch (UnknownHostException e) {
+
+            e.printStackTrace();
+        }
+    }
+
+    public void setIp(String ip){
+
+        ipAddr = ip;
+    }
+    public String getIp(){
+        return ipAddr;
+    }
+
+    public void setHostname(String hostname){
+        this.hostname = hostname;
+    }
+
+    public String getHostname(){
+        return hostname;
+    }
 
 }
