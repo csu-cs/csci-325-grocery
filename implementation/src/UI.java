119f96999663c667f3a3a84b196747a57a38dfa2
MatthewM39 <MCMcCrackin@csustudent.net>
2015-11-25 12:09:53 -0500

    Initial push
    woop woop

--- implementation/src/UI.java
+++ implementation/src/UI.java
@@ -1,4 +1,23 @@
 /**
- * Created by eric on 10/28/2015.
+ * Created by Matthew on 11/24/2015.
  */
-public class UI {} // Handled by Matthew
+
+import javax.swing.*;
+
+
+public class UI {
+
+
+    public static void main(String args[]) {
+        JFrame groceryUI = new JFrame("CSCI 325 Grocery Project");
+        uiManagement myHomeScreen = new uiManagement();
+        groceryUI.add(new uiManagement());
+        groceryUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        groceryUI.pack();
+        groceryUI.setVisible(true);
+
+    }
+
+
+
+}
