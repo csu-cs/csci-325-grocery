d19781028223a93b9cdf4f87ee0f07ade661f473
Eric Comitz <ericcomitz@yahoo.com>
2015-11-26 19:32:52 -0500

    Fixing a thing
    Title

--- implementation/src/listManagement.java
+++ implementation/src/listManagement.java
@@ -3,16 +3,7 @@
 
     public class listManagement{
 
-        public void add(List k, String name, int quantity)
-        {
-
-            Items addedItem = new Items(name, quantity);
-            addedItem.previous = k.head;
-            addedItem.next = null;
-            k.head = addedItem;
-
-        }
-
+     
 
 
 
