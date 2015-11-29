069ec7fb1a76e439b28fb6da17996cec4b3095c9
merrellblack <blackm1@g.cofc.edu>
2015-11-29 13:30:24 -0500

    11/29 --Adding preliminary classes for LAN handling

--- implementation/src/LAN.java
+++ implementation/src/LAN.java
@@ -1,45 +1,48 @@
 import java.net.InetAddress;
-import java.net.UnknownHostException;
 
-public class LAN {
-
-
+public class LAN extends LANManagement{
 
     InetAddress ip;
     String hostname;
     String ipAddr;
+    final int myPort = 130001;
 
     public LAN(){
-
+        startServer(myPort);
     }
 
     // attempt to automatically pull the local ip address and host name
-    public void autoAssignNet() {
+    public void autoAssign() {
         try {
             ip = InetAddress.getLocalHost();
             ipAddr = ip.getHostAddress();
             hostname = ip.getHostName();
-            System.out.println("Your current IP address : " + ip);
-            System.out.println("Your current Hostname : " + hostname);
-
-        } catch (UnknownHostException e) {
-
-            e.printStackTrace();
+           // System.out.println("Your current IP address : " + ip);
+           // System.out.println("Your current Hostname : " + hostname);
+        } catch (Exception e) {
+            System.out.println("Error auto assigning host");
+          //  e.printStackTrace();
         }
     }
 
-    public void setIp(String ip){
+
+    public void setMyIp(String ip){
+
         ipAddr = ip;
     }
+
     public String getIp(){
+
         return ipAddr;
     }
 
-    public void setHostname(String hostname){
+    public void setMyHostname(String hostname){
+
         this.hostname = hostname;
     }
 
-    public String getHostname(){
+    public String getMyHostname(){
+
         return hostname;
     }
 
