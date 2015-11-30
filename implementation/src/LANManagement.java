2fcaf6c9ba192babbd03ae4c6582d79a8814e2cb
blackm1 <merrell.black@gmail.com>
2015-11-30 12:17:36 -0500

    11/30 -LAN resolved null pointer  and porting issues

--- implementation/src/LANManagement.java
+++ implementation/src/LANManagement.java
@@ -9,11 +9,12 @@ import java.net.Socket;
  */
 public class LANManagement {
     final int timeout = 200;
-    LANServers[] serverArray = new LANServers[254];
     int subnet = 14;
     int port = 130001;
     int numServers = 0;
+    LANServers[] serverArray = new LANServers[subnet];
 
+    /**** Starts Local Server (Listening services)****/
     public void startServer(int myPort){
         ServerSocket serverSocket = null;
        try {
@@ -39,21 +40,25 @@ public class LANManagement {
     }
 
     /****Search for listening servers ****/
-    public int searchServers(String hostnet){
+    public String[] searchServers(String hostnet){
         Socket searchSocket;
+        String[] aServers;
+        System.out.println("Searching for servers....");
         for (int i = 0; i <= subnet; i++) {
             String hosting = hostnet+"."+i;
             try {
                 searchSocket = checkSocket(hosting);
-                serverArray[i] = new LANServers(searchSocket);
-                numServers++;
-                System.out.println(hosting + " Active");
-                System.out.println(serverArray[i].getName());
+                if (searchSocket != null) {
+                    serverArray[i] = new LANServers(searchSocket);
+                    numServers++;
+                    System.out.println("Host: "+ serverArray[i].getName()+" server slot: "+ i);
+                }
             }catch (Exception e){
-
+                   // System.err.println( "Host: "+hosting+" NOT AVAILABLE");
             }
         }
-        return numServers;
+        aServers = availableServers();
+        return aServers;
     }
 
     /**** Disconnects all servers ****/
@@ -66,27 +71,38 @@ public class LANManagement {
     /**** Displays available servers: we can use
      * this method to return a list/(object)
      * of listening servers to the UI****/
-    public void availableServers(){
-        for(int i = 0; i <= numServers;i++){
-            System.out.println(serverArray[i].getName()+ "available");
+    public String[] availableServers(){
+        System.out.println(numServers + "of" + serverArray.length + "available");
+        String[] serverList = new String[numServers];
+        int sTracker = 0;
+        if(numServers>0) {
+            for (int i = 0; i < serverArray.length; i++) {
+                if (serverArray[i] != null) {
+                    serverList[sTracker] = serverArray[i].getName();
+                    sTracker++;
+                }
+            }
+            return serverList;
         }
-
+        return null;
     }
 
     /**** Check to see if server is listening ****/
     public Socket checkSocket(String hostServer)throws Exception{
         Socket socket = new Socket();
-        InetSocketAddress host = new InetSocketAddress(hostServer, 10007);
+        InetSocketAddress host = new InetSocketAddress(hostServer, port);
         socket.connect(host, timeout);
         return(socket);
     }
 
     /**** Getters & Setters ****/
     public void setPort(int port){
+
         this.port = port;
     }
 
     public void setSubnet(int subnet){
+
         this.subnet = subnet;
     }
 
