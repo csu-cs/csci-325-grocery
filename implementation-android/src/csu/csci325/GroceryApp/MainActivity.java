8efd186f5936264983b5fdedfc954f9ceec9c19c
Eric Comitz <ericcomitz@yahoo.com>
2015-12-03 14:30:11 -0500

    Uploading stuff
    Title

--- implementation-android/src/csu/csci325/GroceryApp/MainActivity.java
+++ implementation-android/src/csu/csci325/GroceryApp/MainActivity.java
@@ -2,6 +2,11 @@ package csu.csci325.GroceryApp;
 
 import android.app.Activity;
 import android.os.Bundle;
+// import android.view.View;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+
 
 public class MainActivity extends Activity {
     /**
@@ -11,5 +16,48 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+Button button1 = (Button) findViewById(R.id.FirstButton);
+Button button2 = (Button) findViewById(R.id.SecondButton);
+Button button3 = (Button) findViewById(R.id.ThirdButton);
+        ListView theList = (ListView) findViewById(R.id.ListOfItems);
+
+
+button1.setOnClickListener(new View.OnClickListener() {
+    public void onClick(View view){
+        String currentButtonText = button1.getText().toString();
+        if(currentButtonText.equals("Create List"))
+        {
+            button1.setText("Add");
+            button2.setText("Remove");
+            button3.setText("Save");
+            button3.setVisibility(View.VISIBLE);
+            button3.setClickable(true);
+
+        }
+
     }
+});
+
+button2.setOnClickListener(new View.OnClickListener() {
+    public void onClick(View view)
+    {
+        String currentButtonText = button2.getText().toString();
+        if(currentButtonText.equals("View List"))
+        {
+            button1.setText("Edit");
+            button2.setText("Send");
+
+        }
+    }
+});
+
+
+    }
+
+
+    public static void viewListFromFile()
+    {
+
+    }
+
 }
